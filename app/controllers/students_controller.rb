9c7dba317ded992939bb0b71621a6ef5449a8eb2
github username <github email address>
2019-09-20 13:51:11 -0400

    students show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
   def show
+    @student = Student.all.find(params[:id])
+    @this_students_school = School.all.find_by_id(@student.school_id)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   get '/schools', to: "schools#index", as: 'schools'
   get '/schools/:id', to: "schools#show", as: 'school'
-  get '/student/:id', to: "student#show", as: 'student'
+  get '/students/:id', to: "students#show", as: 'student'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
